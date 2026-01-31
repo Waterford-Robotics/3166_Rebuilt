eadba3f82ca5b7f3791dec205e32ab331bc53fd0
deanziesler <infinititedean@gmail.com>
2026-01-31 13:13:40 -0700

    added swerve code to the container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,9 +12,34 @@ public class RobotContainer {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+
+             //resets navX
+        new JoystickButton(m_driverController.getHID(), ControllerConstants.resetNavX)
+        .onTrue(new InstantCommand(
+            () -> m_swerveSubsystem.zeroGyro(),
+            m_swerveSubsystem));
+
+        // strafe right (right trigger)
+        new Trigger(() -> m_driverController.getRawAxis(ControllerConstants.k_righttrig) > 0.05)
+        .whileTrue(new RunCommand(
+            () -> m_swerveSubsystem.robotOrientedDriveCommand(() -> m_driverController.getRawAxis(ControllerConstants.k_righttrig)),
+            m_swerveSubsystem));
+    
+        //strafe left, left trigger
+        new Trigger(() -> m_driverController.getRawAxis(ControllerConstants.k_lefttrig) > 0.05)
+        .whileTrue(new RunCommand(
+            () -> m_swerveSubsystem.robotOrientedDriveCommand(() -> -1*m_driverController.getRawAxis(ControllerConstants.k_lefttrig)),
+            m_swerveSubsystem)); 
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
+
+   Command driveFieldOrientedAngularVelocity = m_swerveSubsystem.driveCommand(
+        () -> MathUtil.applyDeadband(m_driverController.getLeftY() * DriveConstants.k_driveSpeed, DriveConstants.k_driveDeadBand),
+        () -> MathUtil.applyDeadband(m_driverController.getLeftX() * DriveConstants.k_driveSpeed, DriveConstants.k_driveDeadBand),
+        () -> m_driverController.getRightX() * DriveConstants.k_turnRate); 
 }
