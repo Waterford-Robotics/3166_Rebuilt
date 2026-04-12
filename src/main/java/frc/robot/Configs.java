b7fa78eaa660f84bb118486fc57fa6de8b831d1b
QLinux407 <qj108568@waterfordschool.org>
2026-04-12 08:51:57 -0600

    Added Kraken Config file

---
+++ src/main/java/frc/robot/Configs.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.units.Units;
+
+
+public class Configs {
+  public static final class IndexerConfigs {
+    static{
+
+    }
+    
+  }
+  public static final class ShooterConfigs {
+    static{
+      
+    }
+    
+  }
+}
