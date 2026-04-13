447b67896ae63a59e250a3f9885786fa1142503f
QLinux407 <qj108568@waterfordschool.org>
2026-04-13 15:14:57 -0600

    importes form 3245 addded to Config

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -10,6 +10,20 @@ import edu.wpi.first.units.Units;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import org.ejml.equation.VariableType;
+
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.UpdateModeValue;
+
+import edu.wpi.first.units.Units;
+
+
+
 
 
 public class Configs {
@@ -27,7 +41,7 @@ public class Configs {
      public static final TalonFXConfiguration s1Configs = new TalonFXConfiguration();
      public static final TalonFXConfiguration s2Configs = new TalonFXConfiguration();
      static{
-
+  
      }
     
   }
